53c3505493bd86d849fb1c1683773e18dff23bcf
reiken <ultee529@outlook.jp>
2024-09-23 16:55:27 +0900

    tailwindの設定を変更

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,14 @@
 module.exports = {
-    content: ['./src/**/*.{js,jsx,ts,tsx}'],
-    theme: {},
-    plugins: [require('@tailwindcss/forms')],
-}
+    content: [
+      "./app/**/*.{js,ts,jsx,tsx,mdx}",
+      "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./components/**/*.{js,ts,jsx,tsx,mdx}",
+   
+      // Or if using `src` directory:
+      "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
